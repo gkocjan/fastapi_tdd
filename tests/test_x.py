945a73f39417ded26b34e1e5dd9f19425a522e6e
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-27 23:10:08 +0200

    [RED?] Try to write the first real test that is failing

--- tests/test_x.py
+++ tests/test_x.py
@@ -1,5 +1,4 @@
 """
-test_get_sku_for_not_existing_id_returns_404_status_code
 test_get_sku_for_not_existing_id_returns_message_in_detail_field
 test_get_sku_for_existing_id_returns_200_status_code
 test_get_sku_for_existing_id_returns_sku_id_and_name  # can split into 2 tests
@@ -8,6 +7,15 @@ in case of auth:
 test_get_sku_returns_401_for_unauthorized_user
 """
 
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
-def test_x():
-    assert False
+app = FastAPI()
+
+client = TestClient(app)
+
+
+def test_get_sku_for_not_existing_id_returns_404_status_code_and_message_in_detail_field():
+    response = client.get("/sku/NOT_EXISTING")
+
+    assert response.status_code == 404
