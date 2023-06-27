6b1ec5825e8154597baa22a3043d6cef799500a4
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-27 22:54:36 +0200

    Notes with the first series of tests to write

--- tests/test_x.py
+++ tests/test_x.py
@@ -1,2 +1,13 @@
+"""
+test_get_sku_for_not_existing_id_returns_404_status_code
+test_get_sku_for_not_existing_id_returns_message_in_detail_field
+test_get_sku_for_existing_id_returns_200_status_code
+test_get_sku_for_existing_id_returns_sku_id_and_name  # can split into 2 tests
+
+in case of auth:
+test_get_sku_returns_401_for_unauthorized_user
+"""
+
+
 def test_x():
     assert False
