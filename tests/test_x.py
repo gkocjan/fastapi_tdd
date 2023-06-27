ca61c1c2e123480e55a19c154afcec7222fa106b
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-27 23:12:17 +0200

    [RED] Make the first test fail

--- tests/test_x.py
+++ tests/test_x.py
@@ -1,5 +1,4 @@
 """
-test_get_sku_for_not_existing_id_returns_message_in_detail_field
 test_get_sku_for_existing_id_returns_200_status_code
 test_get_sku_for_existing_id_returns_sku_id_and_name  # can split into 2 tests
 
@@ -19,3 +18,4 @@ def test_get_sku_for_not_existing_id_returns_404_status_code_and_message_in_deta
     response = client.get("/sku/NOT_EXISTING")
 
     assert response.status_code == 404
+    assert response.json() == {"detail": "SKU not found"}
