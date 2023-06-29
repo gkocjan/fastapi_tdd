ad7cb09231fc512d583c9f2b6edba80404eea017
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-29 22:42:17 +0200

    [GREEN] Get SKU from db

--- tests/test_x.py
+++ tests/test_x.py
@@ -14,7 +14,9 @@ db = {}
 
 @app.get("/sku/{sku_id}")
 def get_sku_by_id(sku_id: str):
-    raise HTTPException(status_code=404, detail="SKU not found")
+    sku = db.get(sku_id)
+    if sku is None:
+        raise HTTPException(status_code=404, detail="SKU not found")
 
 
 client = TestClient(app)
