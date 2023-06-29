a826975e4058d660556d3a7ce2a9333c5a4b793e
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-29 22:36:01 +0200

    [RED] Test get existing SKU returns status 200

--- tests/test_x.py
+++ tests/test_x.py
@@ -1,5 +1,4 @@
 """
-test_get_sku_for_existing_id_returns_200_status_code
 test_get_sku_for_existing_id_returns_sku_id_and_name  # can split into 2 tests
 
 in case of auth:
@@ -10,6 +9,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.testclient import TestClient
 
 app = FastAPI()
+db = {}
 
 
 @app.get("/sku/{sku_id}")
@@ -25,3 +25,14 @@ def test_get_sku_for_not_existing_id_returns_404_status_code_and_message_in_deta
 
     assert response.status_code == 404
     assert response.json() == {"detail": "SKU not found"}
+
+
+def test_get_sku_for_existing_id_returns_200_status_code():
+    db["TD:4321"] = {
+        "sku_id": "TD:4321",
+        "name": "SKU name 1",
+    }
+
+    response = client.get("/sku/TD:4321")
+
+    assert response.status_code == 200
