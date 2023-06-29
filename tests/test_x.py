5c83c98585b1b8d203b9f5c7e48c2002c94b8d7b
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-29 23:59:50 +0200

    [REFACTOR] Create fixture to create single SKU

--- tests/test_x.py
+++ tests/test_x.py
@@ -1,8 +1,17 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from fastapi_tdd.main import app, db
 
 client = TestClient(app)
+
+
+@pytest.fixture
+def single_SKU_in_DB():
+    db["TD:4321"] = {
+        "sku_id": "TD:4321",
+        "name": "SKU name 1",
+    }
 
 
 def test_get_sku_for_not_existing_id_returns_404_status_code_and_message_in_detail_field():
@@ -12,12 +21,7 @@ def test_get_sku_for_not_existing_id_returns_404_status_code_and_message_in_deta
     assert response.json() == {"detail": "SKU not found"}
 
 
-def test_get_sku_for_existing_id_returns_200_status_code():
-    db["TD:4321"] = {
-        "sku_id": "TD:4321",
-        "name": "SKU name 1",
-    }
-
+def test_get_sku_for_existing_id_returns_200_status_code(single_SKU_in_DB):
     response = client.get("/sku/TD:4321")
 
     assert response.status_code == 200
