5c3f3fbe4e6946f7724a283c71e3bed2d0dc35e5
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-30 00:09:25 +0200

    [REFACTOR] Cleanup in test names

--- tests/test_get_sku_by_id.py
+++ tests/test_get_sku_by_id.py
@@ -20,20 +20,20 @@ def single_SKU_in_DB():
     db.clear()  # clean up after the test
 
 
-def test_get_sku_for_not_existing_id_returns_404_status_code_and_message_in_detail_field():
+def test_not_existing_id_returns_404_status_code_and_message_in_detail_field():
     response = client.get("/sku/NOT_EXISTING")
 
     assert response.status_code == 404
     assert response.json() == {"detail": "SKU not found"}
 
 
-def test_get_sku_for_existing_id_returns_200_status_code(single_SKU_in_DB):
+def test_existing_id_returns_200_status_code(single_SKU_in_DB):
     response = client.get("/sku/TD:4321")
 
     assert response.status_code == 200
 
 
-def test_get_sku_for_existing_id_returns_sku_id_and_name_v1(single_SKU_in_DB):
+def test_existing_id_returns_sku_id_and_name_v1(single_SKU_in_DB):
     response = client.get("/sku/TD:4321")
     assert response.json() == {
         "sku_id": "TD:4321",
@@ -41,7 +41,7 @@ def test_get_sku_for_existing_id_returns_sku_id_and_name_v1(single_SKU_in_DB):
     }
 
 
-def test_get_sku_for_existing_id_returns_sku_id_and_name_v2(single_SKU_in_DB):
+def test_existing_id_returns_sku_id_and_name_v2(single_SKU_in_DB):
     response = client.get("/sku/TD:4321")
     result_sku = response.json()
 
