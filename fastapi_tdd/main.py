76b0a67586f8e7bcdce640b6a0af3c99f006ad47
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-30 15:41:16 +0200

    [REFACTOR] Use FastAPI Depends to access SKURepo

--- fastapi_tdd/main.py
+++ fastapi_tdd/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 
 from fastapi_tdd.repo import SKURepo
@@ -6,9 +6,13 @@ from fastapi_tdd.repo import SKURepo
 app = FastAPI()
 
 
+def get_sku_repo():
+    return SKURepo()
+
+
 @app.get("/sku/{sku_id}")
-def get_sku_by_id(sku_id: str):
-    sku = SKURepo().get(sku_id)
+def get_sku_by_id(sku_id: str, sku_repo: SKURepo = Depends(get_sku_repo)):
+    sku = sku_repo.get(sku_id)
     if sku is None:
         raise HTTPException(status_code=404, detail="SKU not found")
     return sku
@@ -19,8 +23,12 @@ class SKUProductAssignment(BaseModel):
 
 
 @app.put("/sku/{sku_id}")
-def update_sku_by_id(sku_id: str, sku_product_assignement: SKUProductAssignment):
-    sku = SKURepo().get(sku_id)
+def update_sku_by_id(
+    sku_id: str,
+    sku_product_assignement: SKUProductAssignment,
+    sku_repo: SKURepo = Depends(get_sku_repo),
+):
+    sku = sku_repo.get(sku_id)
     if sku is None:
         raise HTTPException(status_code=404, detail="SKU not found")
     sku.product_name = sku_product_assignement.product_name
