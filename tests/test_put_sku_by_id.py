b0feecff0358aa89160bb9439b3d822984c36854
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-30 13:22:02 +0200

    [RED] Test if update existing id returns 200

--- tests/test_put_sku_by_id.py
+++ tests/test_put_sku_by_id.py
@@ -1,7 +1,6 @@
 from fastapi.testclient import TestClient
 
 """
-test_existing_id_and_product_name_in_body_returns_200_status_code
 test_existing_unassigned_id_and_product_name_in_body_returns_assigned_sku
 test_existing_assigned_id_and_product_name_in_body_returns_updated_assigned_sku
 test_existing_assigned_id_and_None_product_name_in_body_unassigns_product_from_sku
@@ -16,3 +15,10 @@ def test_not_existing_id_returns_404_status_code_and_message_in_detail_field(
 
     assert response.status_code == 404
     assert response.json() == {"detail": "SKU not found"}
+
+
+def test_existing_id_and_product_name_in_body_returns_200_status_code(
+    client: TestClient, single_SKU_in_DB
+):
+    response = client.put("/sku/TD:4321", json={"product_nane": "product"})
+    assert response.status_code == 200
