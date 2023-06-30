e563640c0f1f4b4904dd0856062416b6183140b4
Grzegorz Kocjan <grzegorz@kocjan.me>
2023-06-30 13:22:35 +0200

    [GREEN] Check if SKU exists for PUT

--- fastapi_tdd/main.py
+++ fastapi_tdd/main.py
@@ -14,4 +14,6 @@ def get_sku_by_id(sku_id: str):
 
 @app.put("/sku/{sku_id}")
 def update_sku_by_id(sku_id: str):
-    raise HTTPException(status_code=404, detail="SKU not found")
+    sku = db.get(sku_id)
+    if sku is None:
+        raise HTTPException(status_code=404, detail="SKU not found")
